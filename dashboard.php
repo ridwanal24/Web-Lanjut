9c9c19a35d866f6411f929c6dd59b39b43a2a7ce
ridwanal24 <ridwanalakhyar1999@gmail.com>
2019-12-05 20:22:29 +0700

    edit

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<?php
+if (($_SESSION['username'])=="admin") {
+	echo "Silahkan login dulu ";
+}else{
+
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="list-group">
+				<a href="#" class="list-group-item list-group-item-action active">
+					Cras justo odio
+				</a>
+				<a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+				<a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+				<a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+				<a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+}
+?>
--- login_admin_check.php
+++ login_admin_check.php
@@ -4,19 +4,21 @@ include 'layout/header.php';
 <?php
 	 
 	$username = $_POST['username'];
-	$password = ($_POST['password']);
+	$password = $_POST['password'];
 	
-	$login = mysql_query("select * from admin where username='$username' and password='$password'");
+	$login = mysql_query("select * from admin where username='$username' and pass='$password'");
 	$cek = mysql_num_rows($login);
 	
 	if($cek > 0){
 		session_start();
+		
+		
 		$_SESSION['username'] = $username;
-		$_SESSION['status'] = "login";
-		$_SESSION['loginType'];
+		$_SESSION['status'] = "admin";
+		//$_SESSION['loginType'];
 		header("location:dashboard.php");
 	}else{
-		header("location:dashboard.php");	
+		header("location:login_admin.php");	
 	}
 	
 ?>
