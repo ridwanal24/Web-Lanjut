f5cdc661851366336acf58d5ca49013a2a61b427
ridwanal24 <ridwanalakhyar1999@gmail.com>
2019-12-06 10:53:46 +0700

    Dashboard

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,32 +1,33 @@
 <?php
 session_start();
-include '../layout/header.php';
+include 'layout/header.php';
 
-if (!isset($_SESSION['status'])&&$_SESSION['status']=="admin") {
+if (!isset($_SESSION['status'])) {
 	echo "Silahkan login dulu ";
 }else{
 
-?>
+	?>
 
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-4">
-			<div class="list-group">
-				<a href="#" class="list-group-item list-group-item-action active">
-					Cras justo odio
-				</a>
-				<a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-				<a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-				<a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-				<a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+	<div class="container-fluid">
+		<br>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="list-group">
+					<a href="#" class="list-group-item list-group-item-action active">
+						Cras justo odio
+					</a>
+					<a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+					<a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+					<a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+					<a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
+				</div>
+			</div>
+			<div class="col-md-8">			
 			</div>
 		</div>
-		<div class="col-md-8">
-			
-		</div>
 	</div>
-</div>
-<?php
+	<br>
+	<?php
 }
-include '../layout/footer.php';
+include 'layout/footer.php';
 ?>
---
+++ dashboard/layout/header.php
@@ -0,0 +1,71 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Sekolahmu</title>
+
+  <!-- Bootstrap core CSS -->
+  
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/heroic-features.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Dashboard Admin</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          
+              
+              <!-- Change Login Menu to User's Name -->
+              <?php
+                if (!isset($_SESSION['status'])) {
+              ?>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbardropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Login</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="login_admin.php">Admin</a>
+          <a class="dropdown-item" href="login_guru.php">Guru</a>
+          <a class="dropdown-item" href="login_siswa.php">Siswa</a>
+          </li>
+          <?php }
+            else{
+              ?>
+             <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbardropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php
+            echo $_SESSION['username'];
+            ?>
+              
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          
+          <a class="dropdown-item" href="logout.php">Keluar</a>
+          
+          </li>
+              <?php
+            }
+          ?>
+          <!-- End Of Change User's Name -->
+        
+        </ul>
+      </div>
+    </div>
+  </nav>
