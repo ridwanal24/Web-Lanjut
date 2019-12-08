4c3dfd5c7c96effde218fc5fc0c6725663d15d5b
fahsera <mifasera@gmail.com>
2019-12-09 03:21:09 +0700

    kasaran

--- galeri.php
+++ galeri.php
@@ -5,21 +5,26 @@ include 'layout/header.php';
 ?>
 
 <?php
+$db = new mysqli("localhost","root","","db_sekolahmu");
 $sql = "SELECT pathImg, pathVideo FROM galeri ";
-$result = $db->query($sql);
+$sth = $db->query($sql);
 
-if ($result->num_rows > 0) {
-    $id_div=1;
-    while($row = $result->fetch_assoc()) {
-        $pathgambar ='./assets/img/galeri/'.$row['pathImg'];
-        echo $row["pathImg"];
-        $id_div=1+$id_div;
+$file_path = './assets/img/galeri/';
+// if ($sth->num_rows > 0) {
+//     $id_div=1;
+    while($rows = mysqli_fetch_array($sth)) {
+        $img_src = $rows['pathImg'];
+        ?>
 
-    }
-} else {
+        <img src="<?php echo $img_src; ?>" alt="" width="300" height="200"/>
+        <?php
+        }
+    ?>
+<!-- } else {
     echo "0 results";
 }
-?>
+
+?> -->
 
 <?php
 include 'layout/footer.php';
