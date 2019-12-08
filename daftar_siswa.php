b02968d0c42208ccd344643c242783f5ce9bc907
ridwanal24 <ridwanalakhyar1999@gmail.com>
2019-12-08 13:58:43 +0700

    daftar siswa

--- daftar_siswa.php
+++ daftar_siswa.php
@@ -4,38 +4,39 @@ include 'layout/header.php';
 ?>
 
 <div class="container-fluid">
-<center><p class="h1">DAFTAR SISWA</p></center>
-<br>
-<div class="row">
-	<div class="col-md-2"></div>
-	<div class="col-md-8">
-		<table class="table">
-		<thead class="thead-dark">
-			<tr>
-				<th scope="col">No</th>
-				<th scope="col">Nama</th>
-				<th scope="col">Kelas</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<th scope="row">1</th>
-				<td>Budi Setiawan</td>
-				<td>Binomo A</td>
-			</tr>
-			<tr>
-				<th scope="row">2</th>
-				<td>Alan Suryajana</td>
-				<td>Trader A</td>
-			</tr>
+	<br>
+	<center><p class="h1">DAFTAR SISWA</p></center>
+	<br>
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<table class="table table-striped">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">No</th>
+						<th scope="col">Nama</th>
+						<th scope="col">Kelas</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Budi Setiawan</td>
+						<td>Binomo A</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Alan Suryajana</td>
+						<td>Trader A</td>
+					</tr>
 
-		</tbody>
-	</table>	
+				</tbody>
+			</table>	
+		</div>
+		<div class="col-md-2"></div>
 	</div>
-	<div class="col-md-2"></div>
-</div>
 
-	
+<br>	
 </div>
 
 <?php
