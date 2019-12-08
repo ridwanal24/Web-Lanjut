f4115682a00f49bbe9490c3dde0d445a8243fd76
fahsera <mifasera@gmail.com>
2019-12-09 03:24:21 +0700

    kasaran

--- galeri.php
+++ galeri.php
@@ -10,8 +10,7 @@ $sql = "SELECT pathImg, pathVideo FROM galeri ";
 $sth = $db->query($sql);
 
 $file_path = './assets/img/galeri/';
-// if ($sth->num_rows > 0) {
-//     $id_div=1;
+
     while($rows = mysqli_fetch_array($sth)) {
         $img_src = $rows['pathImg'];
         ?>
@@ -20,12 +19,6 @@ $file_path = './assets/img/galeri/';
         <?php
         }
     ?>
-<!-- } else {
-    echo "0 results";
-}
-
-?> -->
-
-<?php
+    <?php
 include 'layout/footer.php';
 ?>
