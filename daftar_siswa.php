a24b3e827f32b6c5df307c82b4c551287d590987
ridwanal24 <ridwanalakhyar1999@gmail.com>
2019-12-08 13:03:31 +0700

    Daftar siswa

--- daftar_siswa.php
+++ daftar_siswa.php
@@ -3,6 +3,40 @@ include 'koneksi.php';
 include 'layout/header.php';
 ?>
 
+<div class="container-fluid">
+<center><p class="h1">DAFTAR SISWA</p></center>
+<br>
+<div class="row">
+	<div class="col-md-2"></div>
+	<div class="col-md-8">
+		<table class="table">
+		<thead class="thead-dark">
+			<tr>
+				<th scope="col">No</th>
+				<th scope="col">Nama</th>
+				<th scope="col">Kelas</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope="row">1</th>
+				<td>Budi Setiawan</td>
+				<td>Binomo A</td>
+			</tr>
+			<tr>
+				<th scope="row">2</th>
+				<td>Alan Suryajana</td>
+				<td>Trader A</td>
+			</tr>
+
+		</tbody>
+	</table>	
+	</div>
+	<div class="col-md-2"></div>
+</div>
+
+	
+</div>
 
 <?php
 include 'layout/footer.php';
--- layout/header.php
+++ layout/header.php
@@ -46,7 +46,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="daftar_siswa.php">
               Daftar Siswa
             </a>
           </li>
