d90c56c4852831388541d7f2adf0b9dcf652bfb8
fahsera <mifasera@gmail.com>
2019-12-09 04:21:47 +0700

    update berantkan

--- galeri.php
+++ galeri.php
@@ -4,21 +4,31 @@ include 'koneksi.php';
 include 'layout/header.php';
 ?>
 
-<?php
-$db = new mysqli("localhost","root","","db_sekolahmu");
-$sql = "SELECT pathImg, pathVideo FROM galeri ";
-$sth = $db->query($sql);
+<div class="container-fluid">
+	<br>
+	<center><p class="h1">GALERI</p></center>
+	<br>
 
-$file_path = './assets/img/galeri/';
-
-    while($rows = mysqli_fetch_array($sth)) {
-        $img_src = $rows['pathImg'];
-        ?>
-
-        <img src="<?php echo $img_src; ?>" alt="" width="300" height="200"/>
+    <br>
+	<div class="row">
+		<div class="col-md-2"></div>
         <?php
-        }
-    ?>
+        $db = new mysqli("localhost","root","","db_sekolahmu");
+        $sql = "SELECT pathImg, pathVideo FROM galeri ";
+        $sth = $db->query($sql);
+
+        $file_path = './assets/img/galeri/';
+
+            while($rows = mysqli_fetch_array($sth)) {
+                $img_src = $rows['pathImg'];
+                ?>
+
+                <img src="<?php echo $img_src;?>" alt="" width="500" height="300"/>
+                <?php
+                    }
+                ?>
+    </div>
+</div>
     <?php
 include 'layout/footer.php';
 ?>
