552eb6ef27d9b1102b392afa2cf1799095079ef6
ridwanal24 <ridwanalakhyar1999@gmail.com>
2019-12-15 10:57:21 +0700

    Update

--- daftar_siswa.php
+++ daftar_siswa.php
@@ -12,8 +12,8 @@ if (isset( $_SESSION['status'])) {
 	<center><p class="h1">DAFTAR SISWA</p></center>
 	<br>
 	<div class="row">
-		<div class="col-md-2"></div>
-		<div class="col-md-8 bg-dark">
+		<div class="col-md-1"></div>
+		<div class="col-md-10 bg-dark">
 			
 			<!-- Form Pilihan Kelas -->
 			<form method="get">
@@ -45,12 +45,12 @@ if (isset( $_SESSION['status'])) {
 			<!-- End of Form Pilihan Kelas --> 
 
 		</div>
-		<div class="col-md-2"></div>
+		<div class="col-md-1"></div>
 	</div>
 	<br>
 	<div class="row">
-		<div class="col-md-2"></div>
-		<div class="col-md-8">
+		<div class="col-md-1"></div>
+		<div class="col-md-10">
 			<table class="table table-striped">
 				<thead class="thead-dark">
 					<tr>
@@ -58,41 +58,47 @@ if (isset( $_SESSION['status'])) {
 						<th scope="col">Nama</th>
 						<th scope="col">Kelas</th>
 						<th scope="col">Jurusan</th>
+						<th scope="col"> </th>
 					</tr>
 				</thead>
 				<tbody>
 
 					<!-- PHP Show Data -->
 					<?php
-					if (/*isset($_GET['submit'])&& */isset($_GET['kelas'])&&isset($_GET['jurusan'])) {
+					if (/*isset($_GET['submit'])&& */isset($_GET['kelas']) AND isset($_GET['jurusan'])) {
 						$kelas = $_GET['kelas'];
 						$jurusan = $_GET['jurusan'];
 					
 
-					$query = "Select siswa.nama, kelas.nama as kelas, jurusan.nama as jurusan from siswa join kelas on siswa.idKelas = kelas.idKelas join jurusan on jurusan.idJurusan = kelas.idJurusan where kelas.nama=".$kelas." AND jurusan.idJurusan=".$jurusan;
+					$query = "Select nis, siswa.nama, kelas.nama as kelas, jurusan.nama as jurusan from siswa join kelas on siswa.idKelas = kelas.idKelas join jurusan on jurusan.idJurusan = kelas.idJurusan where kelas.nama=".$kelas." AND jurusan.idJurusan=".$jurusan;
 					$show=mysql_query($query);
 					$nomor=1;
 					while ($result=mysql_fetch_array($show)) {
 
 						?>
-						<tr>
+						<a href="profil_siswa.php"><tr>
 							<th scope="row"><?php echo $nomor;?></th>
 							<td><?php echo $result['nama'];?></td>
 							<td><?php echo $result['kelas'];?></td>
 							<td><?php echo $result['jurusan'];?></td>
-						</tr>		
+							<td><?php 
+							$nis=$result['nis'];
+							?>
+							<a class="btn btn-light" href="profile_siswa.php?nis=<?php echo $nis;?>">LihatProfil</a>
+							</td>
+						</tr></a>		
 						<?php
 						$nomor++;
 					}
 					}
 					?>
 				
-					
+					<a href="profil_siswa.php?nis="></a>
 					<!-- End of PHP Show Data -->
 				</tbody>
 			</table>	
 		</div>
-		<div class="col-md-2"></div>
+		<div class="col-md-1"></div>
 	</div>
 
 	<br>	
---
+++ profile_siswa.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+include 'koneksi.php';
+include 'layout/header.php';
+if (isset( $_SESSION['status'])) {
+	# code...
+echo $_GET['nis'];
+?>
+
+<table border="1">
+	<tr>
+		<td>PROFIL (NAMA SISWA)</td>
+	</tr>
+	<tr>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+	</tr>
+
+</table>
+
+
+<?php
+}else{
+	?>
+	<br>
+	<center>Silahkan Login Terlebih Dahulu</center>
+	<br>
+	<center><a href="login_siswa.php"><button type="button" class="btn btn-primary text-light">LOGIN SISWA</button></a></center>
+	<br>
+	<center><a href="login_guru.php"><button type="button" class="btn btn-primary text-light">LOGIN GURU</button></a></center>
+	<br>
+	<?php
+}
+include 'layout/footer.php';
+?>
