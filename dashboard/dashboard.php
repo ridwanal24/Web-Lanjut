c4872db8175de54cf565453f1226c4e2028bbec6
ridwanal24 <ridwanalakhyar1999@gmail.com>
2019-12-15 16:20:20 +0700

    Update Dashboard

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2,15 +2,15 @@
 session_start();
 include 'layout/header.php';
 echo "<br>";
-if ((!isset($_SESSION['status'])) && ($_SESSION['status']!="admin")) {
+if ((!isset($_SESSION['status'])) AND ($_SESSION['status']!="admin")) {
 	echo "<center>Silahkan login sebagai admin </center>";
 	?>
 	<center><a href="../index.php"><button type="button" class="btn btn-primary text-light">LOGIN</button></a></center>
 	<?php
-}else if ((isset($_SESSION['status'])) && ($_SESSION['status']=="admin")){
+}else if ((isset($_SESSION['status'])) AND ($_SESSION['status']=="admin")){
 	?>
 
-	<div class="container-fluid">
+<!-- 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-1"></div>
 			<div class="text-center col-md-10">
@@ -32,8 +32,81 @@ if ((!isset($_SESSION['status'])) && ($_SESSION['status']!="admin")) {
 			</div>
 			<div class="col-md-1"></div>
 		</div>
-	</div>
+	</div> -->
+ 	<div class="container-fluid">
+ 		<div class="row text-center">
+<!-- Baris Mulai -->
+  	<div class="col-md-1"></div>
+    <div class="col-md-2 mb-4">
+      <div class="card h-100">
+        <img class="card-img-top" src="img/guru.png" alt="">
+        <div class="card-body">
+          <a class="btn btn-primary card-title" href="guru/">Data Guru</a>
+        </div>
+      </div>
+    </div>
 
+    <div class="col-md-2"></div>
+    <div class="col-md-2 mb-4">
+      <div class="card h-100">
+        <img class="card-img-top" src="img/siswa.png" alt="">
+        <div class="card-body">
+          <a class="btn btn-primary card-title" href="siswa/">Data Siswa</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-2"></div>
+    <div class="col-md-2 mb-4">
+      <div class="card h-100">
+        <img class="card-img-top" src="img/fasilitas.png" alt="">
+        <div class="card-body">
+          <a class="btn btn-primary" href="">Fasilitas</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-1"></div>
+</div>
+<!-- Baris Stop -->
+<br>
+<!-- Baris Mulai 2-->
+  	<div class="row text-center">
+  	<div class="col-md-1"></div>
+    <div class="col-md-2 mb-4">
+      <div class="card h-100">
+        <img class="card-img-top" src="img/pengumuman.png" alt="">
+        <div class="card-body">
+          <a class="btn btn-primary card-title" href="">Pengumuman</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-2"></div>
+    <div class="col-md-2 mb-4">
+      <div class="card h-100">
+        <img class="card-img-top" src="img/artikel" alt="">
+        <div class="card-body">
+        	<br>
+        	<br>
+        	<br>
+          <a class="btn btn-primary card-title" href="siswa/">Artikel</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-2"></div>
+    <div class="col-md-2 mb-4">
+      <div class="card h-100">
+        <img class="card-img-top" src="img/galeri" alt="">
+        <div class="card-body">
+          <a class="btn btn-primary" href="">Galeri</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-1"></div>
+<!-- Baris Stop 2-->
+
+ 	</div>
 	<?php
 }else{
 	echo "<center>Silahkan login sebagai admin </center>";
