e3420185b356f16df743ce93c96b90369298946a
fahsera <mifahsera@gmail.com>
2019-12-16 08:35:26 +0700

    up

--- dashboard/galeri/galeri_aksi_edit.php
+++ dashboard/galeri/galeri_aksi_edit.php
@@ -3,7 +3,7 @@ include "koneksi.php";
 $pathVideo     	= $_POST['pathVideo'];
 $pathImg        = $_POST['pathImg'];
 
-$query=mysql_query("UPDATE galeri SET video=$pathVideo, gbr='$pathImg' WHERE id=$id");
+$query=mysql_query("UPDATE galeri SET video='$pathVideo', gbr='$pathImg' WHERE id=$id");
 {
 			echo '<script language="javascript">
 				  alert ("Data Berhasil Diupdate");
