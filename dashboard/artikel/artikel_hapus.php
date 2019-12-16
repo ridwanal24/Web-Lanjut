bf5c0a790a18592e15bdec1b9f41ffa7afe3bfb2
fahsera <mifahsera@gmail.com>
2019-12-16 08:04:10 +0700

    hps

---
+++ dashboard/artikel/artikel_hapus.php
@@ -0,0 +1,11 @@
+<?php ob_start();
+ include "koneksi.php";
+ mysql_query("delete from artikel where idArtikel='$_GET[idArtikel]'");
+ {
+ 	echo '<script language="javascript">
+ 	alert ("Data Berhasil Dihapus");
+ 	window.location="galeri_data.php";
+ 	</script>';
+ 	exit();
+ }
+ ?>
