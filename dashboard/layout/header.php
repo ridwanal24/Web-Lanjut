2485d245602703bfa0d8a8c8bc2a9ee9473f8c76
ridwanal24 <ridwanalakhyar1999@gmail.com>
2020-01-05 10:58:13 +0700

    Change Dasboard Menu

--- dashboard/layout/header.php
+++ dashboard/layout/header.php
@@ -22,12 +22,12 @@
 
 </head>
 
-<body>
+<body class="bg-light">
 
   <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white shadow fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="index.php">Dashboard Admin</a>
+      <a class="navbar-brand" href="../index.php">Dashboard Admin</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
