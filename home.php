cc3172dbfec4059877446062743699b549a2cfb2
ridwanal24 <ridwanalakhyar1999@gmail.com>
2020-01-05 11:56:45 +0700

    Show Pengumuman

--- home.php
+++ home.php
@@ -1,25 +1,33 @@
 <?php
 session_start();
 include 'layout/header.php';
+include 'koneksi.php';
+$query = "SELECT * FROM pengumuman";
+$action = mysql_query($query);
 ?>
 
 <!-- Page Content -->
 
-  <!-- Jumbotron Header -->
-  
-  <!-- Page Features -->
-  <div class="m-1 row m-5 text-center">
-    <div id="artikel" class="col-8 rounded mr-5 bg-white">Artikel</div>
-    <div class="col-3 rounded">
-      <div class="card">
-        <h5 class="card-header">AGENDA</h5>
-        <div class="card-body">
-          <a class="btn btn-primary" href="daftar_agenda.php">Lihat</a>
-        </div>
+<!-- Jumbotron Header -->
+
+<!-- Page Features -->
+<div class="row m-5 text-center">
+  <div id="artikel" class="col-8 rounded mr-5 bg-white">Artikel</div>
+  <div class="col-3 rounded">
+    <div class="card">
+      <h5 class="card-header">Pengumuman</h5>
+      <div class="text-left card-body">
+        <?php
+          while ($data=mysql_fetch_array($action)) { ?>
+              <small><a href=""><?php echo $data['title']; ?></a></small>
+              <hr>
+          <?php }
+        ?>
       </div>
     </div>
   </div>
-  <!-- /.row -->
+</div>
+<!-- /.row -->
 
 <br>
 <br>
